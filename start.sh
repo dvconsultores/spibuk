3b19bfa7f4e6b784c5018b0cb4c168725960159d
dvconsultores <adominguez@dvconsultores.com>
2025-05-09 14:15:17 -0400

    Refactor Dockerfile to install FortiClient VPN using dpkg and update start.sh for simplified VPN connection handling

--- start.sh
+++ start.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-
-# Connect to FortiClient VPN
-echo "Connecting to FortiClient VPN..."
+# Connect to VPN
 forticlient vpn connect --host 200.74.233.50:10443 --username dvconsult --password 'Aros$**'
 
-# Check if the VPN connection was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to connect to FortiClient VPN. Exiting."
-    exit 1
-fi
-
-echo "VPN connected successfully."
-
-# Run the Python application
-echo "Starting Python application..."
-python3 /app/wf0_main.py
+# Start the application
+python wf0_main.py
